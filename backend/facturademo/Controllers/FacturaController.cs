956939e8cb7641a3e9589ce4775c6a72102aebb9
Alberto Ixchop <albertokanecixchop@gmail.com>
2021-10-12 19:26:08 -0600

    first commit

---
+++ backend/facturademo/Controllers/FacturaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace facturademo.Controllers
+{
+    [Route("factura")]
+    [ApiController]
+    public class Factura : Controller
+    {
+        [HttpGet("hola")]
+        public IActionResult Get()
+        {
+            //return Ok("HOla");
+            //return BadRequest("Error");
+            return Ok(
+                new object[]
+                {
+                    new {id=1,nombre="Object special"}
+                }
+                );
+        }
+
+        [HttpPost]//CREATE
+        public IActionResult CreateFactura()
+        {
+            return Ok("creando factura");
+        }
+
+        [HttpGet]//READ all
+        public IActionResult getFacturas()
+        {
+            return Ok("retornando facturas");
+        }
+        [HttpGet("{id}")]//READ by id
+        public IActionResult getFacturabyid()
+        {
+            return Ok("retornando facturas");
+        }
+
+        [HttpPut]//UPDATE
+        public IActionResult updateFactura()
+        {
+            return Ok("Modificando factura");
+        }
+        [HttpDelete]//DELETE
+        public IActionResult delteFactura()
+        {
+            return Ok("Borrando factura");
+        }
+
+
+    }
+}
---
+++ backend/facturademo/Model/Factura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace facturademo.Model
+{
+    public class Factura
+    {
+        String nit;
+        String nombre;
+        String fecha;
+        long correlativo;
+
+        public void getAllFacturas()
+        {
+
+        }
+    }
+}
